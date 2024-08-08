f051337db10fb2e2fe2d43170bf713cf8e9148ff
carpicoder <webcarpi@gmail.com>
2024-08-08 03:28:42 +0200

    Clase 1

---
+++ Clase 01/js/main.js
@@ -0,0 +1,105 @@
+/* Comentarios
+ que pueden tener
+ muchas líneas */
+
+// Comentario de una línea
+
+//! JavaScript es case sensitive
+//! camelCase
+
+// let nombre = "Carpi";
+// const apellido = "Coder";
+
+// let nombre; // undefined
+// nombre = "Carpi";
+
+// let apellido = "Coder";
+
+// const ANIO_NACIMIENTO = 1995;
+
+//! Tipos de dato en JS
+
+// let nombre = "Carpi"; // String = Cadena de caracteres
+// let espacio = " ";
+// let apellido = "Coder";
+
+// let numero = 1995; // Number = Número
+
+// let suma = 1 + 1; // 2
+
+// let resta = numero - suma; // 1995 - 2 //! 1993
+
+// let nombreCompleto = nombre + " " + apellido; // "Carpi Coder"
+
+//! Prompt, consola y alert
+
+//! consola
+
+// let suma = 1 + 1;
+
+// console.log(suma);
+// console.log("Hola, mundo!");
+
+//? división /
+//? multiplicación *
+
+// let numero1 = 30;
+// let numero2 = 2;
+
+// let division = numero1 / numero2;
+
+// console.log(division);
+
+//! alertas
+
+// console.log("Hola!");
+// alert("Hola, mundo!");
+// console.log("Chau!");
+
+//! prompt
+
+// let nombre = prompt("Ingresá tu nombre");
+
+// let saludo = "Hola, " + nombre + " . Buenas tardes!";
+
+// console.log(saludo);
+
+// console.log(prompt("Ingresá tu nombre"));
+
+
+//! parseInt y parseFloat
+
+// let anioNacimiento = parseInt(prompt("Ingresá tu año de nacimiento"));
+
+// let edad = 2024 - anioNacimiento;
+
+// console.log(edad);
+
+
+// console.log(parseFloat("5000.5"))
+
+
+
+//! EJERCICIOS
+//! Ejemplo 1
+
+// let nombre = prompt("Ingresá tu nombre");
+// let saludo = "Hola, " + nombre + "!";
+// console.log(saludo);
+
+//! Ejemplo 2
+
+// let numero1 = parseInt(prompt("Ingresá un número"));
+// let numero2 = 5;
+// let resultado = numero1 + numero2;
+// console.log(resultado);
+
+// console.log(parseInt(prompt("Ingresá un número")) + 5);
+
+//! Ejemplo 3 ft. Mati Carrizo
+
+// let texto1 = prompt("Ingrese un texto aquí");
+// let texto2 = prompt("Ingrese otro texto aquí");
+// alert(texto1 + " " + texto2);
+
+// alert(prompt("Ingrese un texto aquí")+ " " + prompt("Ingrese otro texto aquí"));
