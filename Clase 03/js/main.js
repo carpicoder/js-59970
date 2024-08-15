9f06ac4571e5434b37a3c21885dce058924629a6
carpicoder <webcarpi@gmail.com>
2024-08-15 03:30:58 +0200

    Clase 3

---
+++ Clase 03/js/main.js
@@ -0,0 +1,132 @@
+//! for
+// inicializador
+// condición
+// iterador / modificador
+
+// let numero = "Alex";
+
+// for (let i = 1; i < 5; i++) {
+
+//     if (i === 2 || i === 3) {
+//         continue;
+//     }
+
+//     console.log(i);
+    
+//     // if (i === 2) {
+//     //     break;
+//     // }
+// }
+
+// console.log("Esto está después del ciclo");
+
+
+// for (let entrada = prompt("Ingresá un dato"); entrada !== "ESC"; entrada = prompt("Ingresá un dato")) {
+//     console.log(entrada);
+// }
+
+
+
+//! while
+// let numero = 50;
+// while (numero < 5) {
+//     console.log(numero);
+//     numero++;
+// }
+
+// let entrada = prompt("Ingresar un dato");
+
+// while (entrada.toUpperCase() !== "ESC") {
+//     alert("El usuario ingresó " + entrada);
+//     entrada = prompt("Ingresar otro dato");
+// }
+
+
+//! do...while
+// let numerito = 50;
+// do {
+//     console.log(numerito);
+//     numerito++;
+// } while (numerito < 5);
+
+// let entrada;
+// do {
+//     entrada = prompt("Ingresá un dato");
+//     if (entrada !== "ESC") {
+//         console.log(entrada);
+//     }
+// } while (entrada !== "ESC");
+
+
+
+//! operadores avanzados
+
+// let edad = 29;
+
+// edad = edad + 1;
+// edad++;
+
+// edad = edad - 1;
+// edad--;
+
+// edad = edad + 2;
+// edad += 2;
+// edad -= 2;
+// edad *= 2;
+// edad /= 2;
+
+// edad = edad + 2;
+// edad += 2;
+
+// console.log(edad);
+
+//! switch
+
+// let dia = parseInt(prompt("Ingresá un día de la semana en número"));
+
+// switch(dia) {
+//     case 1:
+//         console.log("Lunes");
+//         break;
+//     case 2:
+//         console.log("Martes");
+//         break;
+//     case 3:
+//         console.log("Miércoles");
+//         break;
+//     case 4:
+//         console.log("Jueves");
+//         break;
+//     case 5:
+//         console.log("Viernes");
+//         break;
+//     case 6:
+//         console.log("Sábado");
+//         break;
+//     case 7:
+//         console.log("Domingo");
+//         break;
+//     default:
+//         console.log("Día inválido");
+// }
+
+
+
+// if (dia === 1) {
+//     console.log("Lunes");
+// } else if (dia === 2) {
+//     console.log("Martes");
+// } else if (dia === 3) {
+//     console.log("Miércoles");
+// } else if (dia === 4) {
+//     console.log("Jueves");
+// } else if (dia === 5) {
+//     console.log("Viernes");
+// } else if (dia === 6) {
+//     console.log("Sábado");
+// } else if (dia === 7) {
+//     console.log("Domingo");
+// } else {
+//     console.log("Día inválido");
+// }
+
